d41a452905d6cacf657f7dfdb5b9cc280e44524d
ritesh <patilritesh998@gmail.com>
2025-06-10 11:06:25 +0530

    DAY 1

---
+++ JAVA/DAY 1/ritesh.java
@@ -0,0 +1,24 @@
+package Demo15;
+import java.util.*;
+public class ritesh extends Demo15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubS
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter name:");
+		String a=sc.nextLine();
+		
+		System.out.print("Enter City:");
+		String b=sc.nextLine();
+		
+		System.out.print("Enter course: ");
+		String c=sc.nextLine();
+		System.out.println(" Name:"+a);
+		System.out.println(" City:"+b);
+		System.out.println(" Course:"+c);
+		
+		
+	}
+
+}

5a5e9dc2af0d56eaa222a47d5b2ea51e276be0ad
ritesh <patilritesh998@gmail.com>
2025-06-23 15:55:32 +0530

    Java

---
+++ JAVA/Day 15/Practical Assignment/Assignment4.java
@@ -0,0 +1,15 @@
+
+
+public class Assignment4 {
+    public static void main(String[] args) {
+        int n=5;
+        int i=1;
+        while(i<=10){
+            
+            int mul=n*i;
+            System.out.println(mul);
+            i++;
+
+        }
+    }
+}
